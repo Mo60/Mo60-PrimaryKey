e28669f1b107fb4cb231ba1067891a8af25f4a58
Mo Khalil <Hamodiweb@outlook.com>
2023-03-07 13:44:54 +0000

    create table and add valus

---
+++ problem1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE song (
+  id INTEGER PRIMARY KEY,
+  title varchar(100) NOT NULL,
+  artist varchar(100) NOT NULL);
+  -- insert some values
+INSERT INTO song VALUES (1, 'Let it be', 'Beatles');
+INSERT INTO song VALUES (2, 'Hotel California', 'Eagles');
+INSERT INTO song VALUES (3, 'Kashmir', 'Led Zeppelin');
